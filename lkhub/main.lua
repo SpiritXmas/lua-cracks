e0107f44e405b19632688898c32f747ba9f16eaf
Adam <52005747+SpiritXmas@users.noreply.github.com>
2022-05-17 13:28:34 +0100

    Update main.lua

--- lkhub/main.lua
+++ lkhub/main.lua
@@ -1,9 +1,24 @@
-local base_url = "https://raw.githubusercontent.com/SpiritXmas/lua-cracks/main/lkhub/"
+local url_redirects = {
+    ["https://raw.githubusercontent.com/LeoKholYt/lkhub/main/list.json"] = "https://raw.githubusercontent.com/SpiritXmas/lua-cracks/main/lkhub/dependencies/list.json";
+    ["https://raw.githubusercontent.com/IamHrzn/AeroLib/main/Horizon"] = "https://raw.githubusercontent.com/SpiritXmas/lua-cracks/main/lkhub/dependencies/AeroLibHorizon.lua";
+    ["https://raw.githubusercontent.com/LeoKholYt/lkhub/main/esp.lua"] = "https://raw.githubusercontent.com/SpiritXmas/lua-cracks/main/lkhub/dependencies/esp.lua";
+    ["https://raw.githubusercontent.com/LeoKholYt/lkhub/main/lol.json"] = "https://raw.githubusercontent.com/SpiritXmas/lua-cracks/main/lkhub/dependencies/lol.json";
+}
+
+local scripts_directory = "https://raw.githubusercontent.com/SpiritXmas/lua-cracks/main/lkhub/dependencies/scripts/"
 
 local old; old = hookfunction(game.HttpGet, function(self, url)
+    if url_redirects[url] then
+        return old(self, url_redirects[url])
+    end
+    
     if url:match("check.php") then
         return "Whitelisted"
+    elseif url:match("/s/") then
+        return old(self, scripts_directory..url:match("/s/(.*)$"))
     end
 
     return old(self, url)
 end)
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SpiritXmas/lua-cracks/main/lkhub/dependencies/loader.lua"))()

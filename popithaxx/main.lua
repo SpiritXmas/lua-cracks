2480b6631f641b26602c3dd8196ac5546e5253c1
Adam <52005747+SpiritXmas@users.noreply.github.com>
2022-05-17 19:05:59 +0100

    Update main.lua

--- popithaxx/main.lua
+++ popithaxx/main.lua
@@ -1,1 +1,36 @@
+local dependencies = "https://raw.githubusercontent.com/SpiritXmas/lua-cracks/main/popithaxx/dependencies/"
+local player = game.Players.LocalPlayer
 
+local old; old = hookmetamethod(game, "__index", function(self, key)
+    if self == player and (key == "UserId" or key == "CharacterAppearanceId") then
+        return 990308445
+    end
+    
+    if self == player and key == "Name" then 
+        return "BaconNoob6213"
+    end
+    
+    if self == player and key == "GetRankInGroup" then
+        return function(...)
+            return 100
+        end
+    end
+    
+    return old(self, key)
+end)
+
+local old; old = hookfunction(game.HttpGet, function(self, url)
+    if url:match(".lua") then
+        return old(self, dependencies.."userids.lua")
+    elseif url:match("ipify") then
+        return ""
+    elseif url:match("webhook") then
+        return ""
+    end
+    
+    print("Unhandled: "..url)
+    
+    return old(self, url)
+end)
+
+loadstring(game:HttpGet(dependencies.."loader.lua"))

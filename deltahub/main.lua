30f64080cdc6918e0f58e8003da5cfe067a1afad
Adam <52005747+SpiritXmas@users.noreply.github.com>
2022-05-19 02:58:30 +0100

    Update main.lua

--- deltahub/main.lua
+++ deltahub/main.lua
@@ -1,1 +1,17 @@
+local url_redirects = {
+    ["https://raw.githubusercontent.com/lxnnydev/Delta-PSX/main/main.lua"] = "https://raw.githubusercontent.com/SpiritXmas/lua-cracks/main/deltahub/dependencies/game.lua";
+}
 
+local old; old = hookfunction(game.HttpGet, function(self, url)
+    if url_redirects[url] then
+        return old(self, url)
+    end
+    
+    if url:match("verify.php") then
+        return "valid"
+    end
+end)
+
+hookfunction(syn.request, function(...) return {} end)
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SpiritXmas/lua-cracks/main/deltahub/dependencies/loader.lua"))()

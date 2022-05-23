a78635f53447da762037af6073f2be9e76bc6b8d
Adam <52005747+SpiritXmas@users.noreply.github.com>
2022-05-23 23:44:30 +0100

    Update main.lua

--- srmhub/main.lua
+++ srmhub/main.lua
@@ -1,1 +1,9 @@
+local old; old = hookmetamethod(game, "__index", function(self, index)
+    if tostring(self) == "KeyBox" and index == "Text" then
+        return "gaynite"
+    end 
+    
+    return old(self, index)
+end)
 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SpiritXmas/lua-cracks/main/srmhub/dependencies/loader.lua"))()

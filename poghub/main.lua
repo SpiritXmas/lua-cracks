aaeb0f0532120ffbea598038614f90d08fcb51fc
Adam <52005747+SpiritXmas@users.noreply.github.com>
2022-05-23 20:54:18 +0100

    Update main.lua

--- poghub/main.lua
+++ poghub/main.lua
@@ -1,1 +1,12 @@
+local old; old = hookmetamethod(game, "__index", function(self, index)
+    if tostring(self) == "Keyinput" and index == "Text" then
+        return "Pog"
+    end
+    
+    return old(self, index)
+end)
 
+hookfunction(writefile, function(...) return end)
+hookfunction(syn.request, function(...) return {} end)
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SpiritXmas/lua-cracks/main/poghub/dependencies/keyloader.lua"))()

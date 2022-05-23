91bb282302b3888496ca1026d051082cfc1fcb2d
Adam <52005747+SpiritXmas@users.noreply.github.com>
2022-05-23 23:51:21 +0100

    Update main.lua

--- srmhub/main.lua
+++ srmhub/main.lua
@@ -1,3 +1,9 @@
+local old; old = hookfunction(game.HttpGet, function(self, url)
+    if url == "https://raw.githubusercontent.com/AikaV3rm/UiLib/master/Lib.lua" then
+        return old(self, "https://raw.githubusercontent.com/SpiritXmas/lua-cracks/main/srmhub/dependencies/uilib.lua")
+    end
+end)
+
 local old; old = hookmetamethod(game, "__index", function(self, index)
     if tostring(self) == "KeyBox" and index == "Text" then
         return "gaynite"

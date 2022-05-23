f4e84c6f8a932a078a5380f99a88e3243160ba6e
Adam <52005747+SpiritXmas@users.noreply.github.com>
2022-05-23 21:13:03 +0100

    Update loader.lua

--- poghub/dependencies/loader.lua
+++ poghub/dependencies/loader.lua
@@ -22,80 +22,8 @@
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/SpiritXmas/lua-cracks/main/poghub/dependencies/garbage.lua"))()
 
-local Pl = game.PlaceId
-if Pl == 5327880096 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/03koios/OnlineBusinessSimulator2/main/README.md"))()
-end
+local a, b = pcall(game.HttpGet, "", "https://raw.githubusercontent.com/SpiritXmas/lua-cracks/main/poghub/dependencies/scripts/"..game.PlaceId..".lua")
 
-if Pl == 6904735821 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/03koios/ShoeSimulator/main/README.md"))()   
-end
-
-if Pl == 1962086868 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/03koios/TowerOfHell/main/README.md"))()
-end   
-
-if Pl == 4954752502 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/03koios/TowerOfMistery/main/README.md"))()
-end
-
-if Pl == 5902977746 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/03sAlt/UltimateTowerDefense/main/README.md"))()
-end 
-
-if Pl == 8884433153 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/03koios/CollectAllPets/main/README.md"))()
-end 
-
-if Pl == 4003872968 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/03koios/GiantSurvival/main/README.md"))()
-end 
-
-if Pl == 189707 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/03koios/NaturalDisasterSurvival/main/README.md"))()
-end 
-
-if Pl == 3101667897 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/03koios/LegendsOfSpeedOP/main/README.md"))()
-end 
-
-if Pl == 5326405001 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/03koios/BaseBattles/main/README.md"))()
-end
-
-
-if Pl == 6284583030 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/03koios/PetSimulatorx/main/PetSimulatorXBobux"))()
-end
-
-if Pl == 734159876 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/03koios/SharkBite/main/BabyShark"))()
-end
-
-if Pl == 5712833750 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/03koios/AnimalSimulator/main/README.md"))()
-end
-
-if Pl == 318978013 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/03koios/KickOff/main/README.md"))()
-end
-
-if Pl == 1554960397 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/03koios/CarDealershipTycoon/main/README.md"))()
-end
-
-if Pl == 2248408710 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/03koios/JaffsisGud/main/README.md"))()
-end
-
-if Pl == 662417684 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/03sAlt/LuckyBlocksBattlegrounds/main/README.md"))()
-end
-
-if Pl == 7993293100 then
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/03sAlt/TsunamiGame/main/README.md'))()
-end
-
-if Pl == 6783611814 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/03sAlt/BuildaBoattoSurvive/main/README.md"))()
+if a then
+    loadstring(b)()
 end

952659040568df35ab5cf5fd2966a0d946719c62
Adam <52005747+SpiritXmas@users.noreply.github.com>
2022-05-23 23:43:36 +0100

    Update loader.lua

--- srmhub/dependencies/loader.lua
+++ srmhub/dependencies/loader.lua
@@ -1,45 +1,7 @@
-repeat wait() until game:IsLoaded()
+local a, b = pcall(game.HttpGet, "", "https://raw.githubusercontent.com/SpiritXmas/lua-cracks/main/srmhub/dependencies/scripts/"..game.PlaceId..".lua")
 
-if game.PlaceId == 5293755937 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/bebedi15/Speed-Run-Simulator/main/Bebedi9960/srmpro.lua"))()
+if a then
+    loadstring(b)()
 else
-    if game.PlaceId == 8069117419 then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/bebedi15/SRM-Scripts/main/Bebedi9960/DemonSoulSimulator.lua"))()
-    else
-        if game.PlaceId == 3101667897 then
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/bebedi15/SRM-Scripts/main/Bebedi9960/LegendsOfSpeed.lua"))()
-        else
-            if game.PlaceId == 5802642341 then
-                loadstring(game:HttpGet("https://raw.githubusercontent.com/bebedi15/SRM-Scripts/main/Bebedi9960/CityLife.lua"))()
-            else
-                if game.PlaceId == 7952502098 then
-                    loadstring(game:HttpGet("https://raw.githubusercontent.com/bebedi15/SRM-Scripts/main/Bebedi9960/impossibleGlassBridge"))()
-                else
-                    if game.PlaceId == 662417684 then
-                        loadstring(game:HttpGet("https://raw.githubusercontent.com/bebedi15/SRM-Scripts/main/Bebedi9960/luckyblocks.lua"))()
-                    else
-                        if game.PlaceId == 8528736393 then
-                            loadstring(game:HttpGet("https://raw.githubusercontent.com/bebedi15/SRM-Scripts/main/Bebedi9960/beatland.lua"))()
-                        else
-                            if game.PlaceId == 7603178367 then
-                                loadstring(game:HttpGet("https://raw.githubusercontent.com/bebedi15/SRM-Scripts/main/Bebedi9960/ChipotleBurritoBuilder"))()
-                            else
-                                if game.PlaceId == 5024793931 then
-                                    loadstring(game:HttpGet"https://raw.githubusercontent.com/bebedi15/SRM-Scripts/main/Bebedi9960/ZeppelinWars")()
-                                else
-                                    if game.PlaceId == 6875469709 then
-                                        loadstring(game:HttpGet"https://raw.githubusercontent.com/bebedi15/SRM-Scripts/main/Bebedi9960/strongestpunchsim.lua")()
-                                    else
-                                        if game.PlaceId == 6459707978 then
-                                            loadstring(game:HttpGet"https://raw.githubusercontent.com/bebedi15/SRM-Scripts/main/Bebedi9960/deliverysim")()
-                                        end
-                                    end
-                                end
-                            end
-                        end
-                    end
-                end
-            end
-        end
-    end
+    game:GetService("Players").LocalPlayer:Kick("Game not supported.")
 end

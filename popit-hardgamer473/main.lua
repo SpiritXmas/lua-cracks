738f9882f648f64c5778bd111d5d00bdd7fb020d
Adam <52005747+SpiritXmas@users.noreply.github.com>
2022-05-28 23:45:29 +0100

    Update main.lua

--- popit-hardgamer473/main.lua
+++ popit-hardgamer473/main.lua
@@ -1,1 +1,9 @@
+local old; old = hookfunction(game.HttpGet, function(self, url)
+    if url == "https://pastebin.com/raw/NsieY5J7" then
+        return string.gsub("return {'urclientid'}", "urclientid", game:GetService("RbxAnalyticsService"):GetClientId())
+    else
+        return old(self, "https://raw.githubusercontent.com/SpiritXmas/lua-cracks/main/popit-hardgamer473/dependencies/kavoui.lua")
+    end
+end)
 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SpiritXmas/lua-cracks/main/popit-hardgamer473/dependencies/loader.lua"))()
